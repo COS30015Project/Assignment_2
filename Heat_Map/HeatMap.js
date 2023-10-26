56ca2a94e6cf846b2eeec65496355d961a5358be
OowanoO <zhenyaowan@gmail.com>
2023-10-26 18:57:49 +0800

    commit

--- Heat_Map/HeatMap.js
+++ Heat_Map/HeatMap.js
@@ -1,14 +1,13 @@
 function init() {
-    const width = 1200; // Width of the SVG
-    const height = 1000; // Height of the SVG
+    const width = 1200;
+    const height = 1000;
 
     const colorScheme = ["#fbb4ae", "#b3cde3", "#ccebc5", "#decbe4", "#fed9a6", "#ffffcc", "#e5d8bd", "#fddaec", "#f2f2f2"];
 
-    const projection = d3.geoAlbersUsa() // Use Albers USA projection for the US map
-        .scale(1000) // Adjust the scale as needed
+    const projection = d3.geoAlbersUsa()
+        .scale(1000)
         .translate([width / 2, height / 2]);
 
-    // Set up the path generator
     const path = d3.geoPath()
         .projection(projection);
 
@@ -19,86 +18,83 @@ function init() {
 
     const g = svg.append("g");
 
-    const color = d3.scaleOrdinal().range(colorScheme); // Define the color scale
+    const color = d3.scaleOrdinal().range(colorScheme);
 
-    // Load the GeoJSON data
-    d3.json("usa.json").then(function (json) { // Replace "usa.json" with the path to your GeoJSON file
-        // Load the CSV data containing the immigration numbers
-        d3.csv("us_migration_data.csv", function (data) {
-            // Modify the data to match the map's features
-            const processedData = {};
-            data.forEach(function (d) {
-                const stateName = d['US States'];
-                processedData[stateName] = {
-                    Bangladesh: +d.Bangladesh,
-                    China: +d.China,
-                    India: +d.India,
-                    Iran: +d.Iran,
-                    Korea: +d.Korea,
-                    Pakistan: +d.Pakistan,
-                    Philippines: +d.Philippines,
-                    Taiwan: +d.Taiwan,
-                    Vietnam: +d.Vietnam,
-                    Others: +d.Others,
-                };
+    Promise.all([
+        d3.json("usa.json"),  // Load GeoJSON data
+        d3.csv("immigration_data.csv")  // Load CSV data
+    ]).then(function (data) {
+        const json = data[0];
+        const csvData = data[1];
+
+        const processedData = {};
+        csvData.forEach(function (d) {
+            const stateName = d['US States'];
+            processedData[stateName] = {
+                Bangladesh: +d.Bangladesh,
+                China: +d.China,
+                India: +d.India,
+                Iran: +d.Iran,
+                Korea: +d.Korea,
+                Pakistan: +d.Pakistan,
+                Philippines: +d.Philippines,
+                Taiwan: +d.Taiwan,
+                Vietnam: +d.Vietnam,
+                Others: +d.Others,
+            };
+        });
+
+        g.selectAll("path")
+            .data(json.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr("class", "feature")
+            .style("fill", function (d) {
+                return color(d.properties.NAME);
+            })
+            .on("click", clicked)
+            .append("title")
+            .text(function (d) {
+                const stateName = d.properties.NAME;
+                const data = processedData[stateName];
+                const formattedData = formatData(data);
+                return stateName + "\n" + formattedData;
             });
 
-            g.selectAll("path")
-                .data(json.features)
-                .enter()
-                .append("path")
-                .attr("d", path)
-                .attr("class", "feature") // Use a class for styling
-                .style("fill", function (d) {
-                    return color(d.properties.NAME);
-                })
-                .on("click", clicked)
-                .append("title") // Add a title element for showing state name and data
-                .text(function (d) {
-                    const stateName = d.properties.NAME;
-                    const data = processedData[stateName];
-                    const formattedData = formatData(data);
-                    return stateName + "\n" + formattedData;
-                });
+        function reset() {
+            g.selectAll(".feature").style("fill", function (d) {
+                return color(d.properties.NAME);
+            });
+            g.transition().call(zoom.transform, d3.zoomIdentity);
+        }
 
-            // Define a reset function for zoom
-            function reset() {
-                g.selectAll(".feature").style("fill", function (d) {
-                    return color(d.properties.NAME);
-                });
-                g.transition().call(zoom.transform, d3.zoomIdentity);
-            }
+        function zoomed(event) {
+            g.attr("transform", event.transform);
+        }
 
-            // Define the zoomed function
-            function zoomed(event) {
-                g.attr("transform", event.transform);
-            }
+        const zoom = d3.zoom()
+            .scaleExtent([1, 8])
+            .on("zoom", zoomed);
 
-            const zoom = d3.zoom()
-                .scaleExtent([1, 8])
-                .on("zoom", zoomed);
+        svg.call(zoom);
 
-            svg.call(zoom); // Call the zoom function
+        function clicked(event, d) {
+            const [[x0, y0], [x1, y1]] = path.bounds(d);
+            event.stopPropagation();
+            reset();
+            d3.select(this).style("fill", "red");
+            svg.transition().duration(750).call(
+                zoom.transform,
+                d3.zoomIdentity
+                    .translate(width / 2, height / 2)
+                    .scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
+                    .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
+                d3.pointer(event, svg.node())
+            );
+        }
 
-            // Handle click events
-            function clicked(event, d) {
-                const [[x0, y0], [x1, y1]] = path.bounds(d);
-                event.stopPropagation();
-                reset(); // Reset colors first
-                d3.select(this).style("fill", "red"); // Highlight the clicked area
-                svg.transition().duration(750).call(
-                    zoom.transform,
-                    d3.zoomIdentity
-                        .translate(width / 2, height / 2)
-                        .scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
-                        .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
-                    d3.pointer(event, svg.node())
-                );
-            }
-
-            // Attach a click event to reset zoom
-            svg.on("click", reset);
-        });
+        svg.on("click", reset);
     });
 }
 
@@ -110,5 +106,4 @@ function formatData(data) {
     return formattedData.join("\n");
 }
 
-// Call the init function when the window loads
 window.onload = init;
