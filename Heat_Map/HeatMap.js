90cee6e255c856c7a1cb34787f0eca8d57bf89be
OowanoO <zhenyaowan@gmail.com>
2023-10-26 15:24:09 +0800

    commit

--- Heat_Map/HeatMap.js
+++ Heat_Map/HeatMap.js
@@ -1,27 +1,34 @@
 // Define a function to initialize the map
 function initMap() {
-    var w = 800; // Width of the SVG
-    var h = 500; // Height of the SVG
+    const width = 800; // Width of the SVG
+    const height = 500; // Height of the SVG
 
-    var colorScheme = ["#fbb4ae", "#b3cde3", "#ccebc5", "#decbe4", "#fed9a6", "#ffffcc", "#e5d8bd", "#fddaec", "#f2f2f2"];
+    const colorScheme = ["#fbb4ae", "#b3cde3", "#ccebc5", "#decbe4", "#fed9a6", "#ffffcc", "#e5d8bd", "#fddaec", "#f2f2f2"];
 
-    var projection = d3.geoAlbersUsa() // Use Albers USA projection for the US map
+    const projection = d3.geoAlbersUsa() // Use Albers USA projection for the US map
         .scale(1000) // Adjust the scale as needed
-        .translate([w / 2, h / 2]);
+        .translate([width / 2, height / 2]);
 
     // Set up the path generator
-    var path = d3.geoPath()
+    const path = d3.geoPath()
         .projection(projection);
 
-    var svg = d3.select("#chart")
+    const svg = d3.select("#chart")
         .append("svg")
-        .attr("width", w)
-        .attr("height", h);
+        .attr("width", width)
+        .attr("height", height);
 
-    var g = svg.append("g");
+    const g = svg.append("g");
 
-    var color = d3.scaleOrdinal().range(colorScheme); // Define the color scale
+    const color = d3.scaleOrdinal().range(colorScheme); // Define the color scale
 
+    const zoom = d3.zoom()
+        .scaleExtent([1, 8])
+        .on("zoom", zoomed);
+
+    svg.call(zoom);
+
+    // Load the GeoJSON data
     d3.json("usa.json").then(function (json) { // Replace "usa.json" with the path to your GeoJSON file
         g.selectAll("path")
             .data(json.features)
@@ -31,7 +38,8 @@ function initMap() {
             .attr("class", "feature") // Use a class for styling
             .style("fill", function (d, i) {
                 return color(i % colorScheme.length); // Apply the color scheme
-            });
+            })
+            .on("click", clicked);
 
         // Define a reset function for zoom
         function reset() {
@@ -43,8 +51,22 @@ function initMap() {
             g.attr("transform", event.transform);
         }
 
-        // Attach a click event to reset zoom
-        svg.on("click", reset);
+        // Handle click events
+        function clicked(event, d) {
+            const [[x0, y0], [x1, y1]] = path.bounds(d);
+            event.stopPropagation();
+            g.selectAll(".feature").style("fill", function (feature) {
+                return feature === d ? "red" : null;
+            });
+            svg.transition().duration(750).call(
+                zoom.transform,
+                d3.zoomIdentity
+                    .translate(width / 2, height / 2)
+                    .scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
+                    .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
+                d3.pointer(event, svg.node())
+            );
+        }
     });
 }
 
