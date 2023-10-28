f8a72c44e9d5dd1d7048e19b2f494153b3ac50c1
OowanoO <zhenyaowan@gmail.com>
2023-10-28 14:59:42 +0800

    commit

--- Heat_Map/HeatMap.js
+++ Heat_Map/HeatMap.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
         .attr('height', height);
 
     // Create a tooltip element for displaying state information.
-    const tooltip = d3.select('#chart') // Use '#chart' instead of '#tooltip'
+    const tooltip = d3.select('#chart')
         .append('div')
         .attr('id', 'tooltip')
         .style('display', 'none')
@@ -23,38 +23,53 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Load the US GeoJSON data.
     d3.json('usa.json').then(function (usData) {
-        // Create a GeoProjection and path.
-        const projection = d3.geoAlbersUsa();
-        const path = d3.geoPath().projection(projection);
-
-        // Bind the GeoJSON data to the SVG and draw the map.
-        svg.selectAll('path')
-            .data(usData.features)
-            .enter()
-            .append('path')
-            .attr('d', path)
-            .attr('fill', 'steelblue')
-            .attr('stroke', 'white')
-            .on('mousemove', function (d) {
-                const stateName = d.properties.NAME;
-                tooltip.style('display', 'block');
-                tooltip.html(stateName); // Modify this line to display the desired information
-
-                // Update the tooltip's position.
-                const [x, y] = d3.pointer(d, this); // Updated to use d3.pointer
-                tooltip.style('left', x + 'px')
-                    .style('top', y + 'px');
-            })
-            .on('mouseout', function () {
-                tooltip.style('display', 'none');
+        // Load the US state data from the CSV.
+        d3.csv('us_state_data.csv').then(function (stateData) {
+            // Create a dictionary to map state names to their data.
+            const stateDataMap = {};
+            stateData.forEach(function (row) {
+                stateDataMap[row['US States']] = row;
             });
 
-        // Add an 'mousemove' event listener to the SVG container.
-        svg.on('mousemove', function () {
-            // Update the tooltip's position.
-            const [x, y] = d3.pointer(this); // Updated to use d3.pointer
-            tooltip.style('left', x + 'px')
-                .style('top', y + 'px');
+            // Create a GeoProjection and path.
+            const projection = d3.geoAlbersUsa();
+            const path = d3.geoPath().projection(projection);
+
+            // Bind the GeoJSON data to the SVG and draw the map.
+            svg.selectAll('path')
+                .data(usData.features)
+                .enter()
+                .append('path')
+                .attr('d', path)
+                .attr('fill', 'steelblue')
+                .attr('stroke', 'white')
+                .on('mousemove', function (d) {
+                    if (d.properties && d.properties.NAME) {
+                        const stateName = d.properties.NAME;
+                        const stateInfo = stateDataMap[stateName];
+                        if (stateInfo) {
+                            const totalAmount = stateInfo['Total'];
+                            tooltip.style('display', 'block');
+                            tooltip.html(`${stateName}<br>Total Amount: ${totalAmount}`);
+                            
+                            // Update the tooltip's position.
+                            const [x, y] = d3.pointer(d, this);
+                            tooltip.style('left', x + 'px')
+                                .style('top', y + 'px');
+                        }
+                    }
+                })
+                .on('mouseout', function () {
+                    tooltip.style('display', 'none');
+                });
+
+            // Add an 'mousemove' event listener to the SVG container.
+            svg.on('mousemove', function () {
+                // Update the tooltip's position.
+                const [x, y] = d3.pointer(this);
+                tooltip.style('left', x + 'px')
+                    .style('top', y + 'px');
+            });
         });
     });
 });
