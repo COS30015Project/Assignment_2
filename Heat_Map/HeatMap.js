9176eb0048be5ad74a2b58d9b4c7c410d37e0305
OowanoO <zhenyaowan@gmail.com>
2023-10-28 13:04:51 +0800

    commit

--- Heat_Map/HeatMap.js
+++ Heat_Map/HeatMap.js
@@ -1,63 +1,72 @@
-function init()
-{
-
-// Define the dimensions of the SVG canvas.
-const width = 800;
-const height = 600;
-
-// Append an SVG element to the body of the page.
-const svg = d3.select('#chart')
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height);
-
-// Load the US GeoJSON data.
-d3.json('usa.json').then(function (usData) {
-  // Create a GeoProjection and path.
-  const projection = d3.geoAlbersUsa();
-  const path = d3.geoPath().projection(projection);
-
-  // Bind the GeoJSON data to the SVG and draw the map.
-  svg.selectAll('path')
-    .data(usData.features)
-    .enter()
-    .append('path')
-    .attr('d', path)
-    .attr('fill', 'steelblue')
-    .attr('stroke', 'white');
-
-  // Load the US migration data CSV.
-  d3.csv('us_migration_data.csv').then(function (migrationData) {
-    // Group migration data by state and calculate total area.
-    const stateAreas = {};
-    migrationData.forEach(function (row) {
-      stateAreas[row['US States']] = {
-        totalArea: row['Total'],
-      };
+function init() {
+    // Define the dimensions of the SVG canvas.
+    const width = 800;
+    const height = 600;
+  
+    // Define color scale for migration totals.
+    const colorScale = d3.scaleSequential(d3.interpolateYlGnBu).domain([0, 30000]);
+  
+    // Append an SVG element to the body of the page.
+    const svg = d3.select('#chart')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height);
+  
+    // Load the US GeoJSON data.
+    d3.json('usa.json').then(function (usData) {
+      // Create a GeoProjection and path.
+      const projection = d3.geoAlbersUsa();
+      const path = d3.geoPath().projection(projection);
+  
+      // Load the US migration data CSV.
+      d3.csv('us_migration_data.csv').then(function (migrationData) {
+        // Group migration data by state and calculate total area.
+        const stateAreas = {};
+        migrationData.forEach(function (row) {
+          stateAreas[row['US States']] = {
+            totalArea: +row['Total'], // Convert to a number
+          };
+        });
+  
+        // Calculate the domain for the color scale.
+        const maxTotal = d3.max(migrationData, (d) => +d['Total']);
+        colorScale.domain([0, maxTotal]);
+  
+        // Bind the GeoJSON data to the SVG and draw the map with colored states.
+        svg.selectAll('path')
+          .data(usData.features)
+          .enter()
+          .append('path')
+          .attr('d', path)
+          .attr('fill', function (d) {
+            const stateName = d.properties.NAME;
+            const totalArea = stateAreas[stateName].totalArea;
+            return colorScale(totalArea);
+          })
+          .attr('stroke', 'white');
+  
+        // Display the total area for each state.
+        svg.selectAll('text')
+          .data(usData.features)
+          .enter()
+          .append('text')
+          .attr('x', function (d) {
+            return path.centroid(d)[0];
+          })
+          .attr('y', function (d) {
+            return path.centroid(d)[1];
+          })
+          .text(function (d) {
+            const stateName = d.properties.NAME;
+            const totalArea = stateAreas[stateName].totalArea;
+            return `State: ${stateName}, Area: ${totalArea}`;
+          })
+          .attr('text-anchor', 'middle')
+          .attr('dy', '0.35em')
+          .attr('fill', 'black');
+      });
     });
-
-    // Display the total area for each state.
-    svg.selectAll('text')
-      .data(usData.features)
-      .enter()
-      .append('text')
-      .attr('x', function (d) {
-        return path.centroid(d)[0];
-      })
-      .attr('y', function (d) {
-        return path.centroid(d)[1];
-      })
-      .text(function (d) {
-        const stateName = d.properties.NAME;
-        return `State: ${stateName}, Area: ${stateAreas[stateName].totalArea}`;
-      })
-      .attr('text-anchor', 'middle')
-      .attr('dy', '0.35em')
-      .attr('fill', 'black');
-  });
-});
-
-
-}
-
-window.onload = init;
+  }
+  
+  window.onload = init;
+  
