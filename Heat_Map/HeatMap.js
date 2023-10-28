25be4c6e239f6373dff359146a6cd5783b2231ac
OowanoO <zhenyaowan@gmail.com>
2023-10-28 12:17:47 +0800

    commit

--- Heat_Map/HeatMap.js
+++ Heat_Map/HeatMap.js
@@ -2,9 +2,13 @@ function init() {
     const width = 1200;
     const height = 1000;
 
-    const colorScheme = [
-        "#fbb4ae", "#b3cde3", "#ccebc5", "#decbe4", "#fed9a6", "#ffffcc", "#e5d8bd", "#fddaec", "#f2f2f2"
-    ];
+    var trans = 60;
+    var width_slider = 920;
+    var height_slider = 50;
+
+    const colorScheme = [   
+        "#0d0887","#100788","#130789","#16078a","#19068c","#1b068d","#1d068e","#20068f","#220690","#240691","#260591","#280592","#2a0593","#2c0594","#2e0595","#2f0596","#310597","#330597","#350498","#370499","#38049a","#3a049a","#3c049b","#3e049c","#3f049c","#41049d","#43039e","#44039e","#46039f","#48039f","#4903a0","#4b03a1","#4c02a1","#4e02a2","#5002a2","#5102a3","#5302a3","#5502a4","#5601a4","#5801a4","#5901a5","#5b01a5","#5c01a6","#5e01a6","#6001a6","#6100a7","#6300a7","#6400a7","#6600a7","#6700a8","#6900a8","#6a00a8","#6c00a8","#6e00a8","#6f00a8","#7100a8","#7201a8","#7401a8","#7501a8","#7701a8","#7801a8","#7a02a8","#7b02a8","#7d03a8","#7e03a8","#8004a8","#8104a7","#8305a7","#8405a7","#8606a6","#8707a6","#8808a6","#8a09a5","#8b0aa5","#8d0ba5","#8e0ca4","#8f0da4","#910ea3","#920fa3","#9410a2","#9511a1","#9613a1","#9814a0","#99159f","#9a169f","#9c179e","#9d189d","#9e199d","#a01a9c","#a11b9b","#a21d9a","#a31e9a","#a51f99","#a62098","#a72197","#a82296","#aa2395","#ab2494","#ac2694","#ad2793","#ae2892","#b02991","#b12a90","#b22b8f","#b32c8e","#b42e8d","#b52f8c","#b6308b","#b7318a","#b83289","#ba3388","#bb3488","#bc3587","#bd3786","#be3885","#bf3984","#c03a83","#c13b82","#c23c81","#c33d80","#c43e7f","#c5407e","#c6417d","#c7427c","#c8437b","#c9447a","#ca457a","#cb4679","#cc4778","#cc4977","#cd4a76","#ce4b75","#cf4c74","#d04d73","#d14e72","#d24f71","#d35171","#d45270","#d5536f","#d5546e","#d6556d","#d7566c","#d8576b","#d9586a","#da5a6a","#da5b69","#db5c68","#dc5d67","#dd5e66","#de5f65","#de6164","#df6263","#e06363","#e16462","#e26561","#e26660","#e3685f","#e4695e","#e56a5d","#e56b5d","#e66c5c","#e76e5b","#e76f5a","#e87059","#e97158","#e97257","#ea7457","#eb7556","#eb7655","#ec7754","#ed7953","#ed7a52","#ee7b51","#ef7c51","#ef7e50","#f07f4f","#f0804e","#f1814d","#f1834c","#f2844b","#f3854b","#f3874a","#f48849","#f48948","#f58b47","#f58c46","#f68d45","#f68f44","#f79044","#f79143","#f79342","#f89441","#f89540","#f9973f","#f9983e","#f99a3e","#fa9b3d","#fa9c3c","#fa9e3b","#fb9f3a","#fba139","#fba238","#fca338","#fca537","#fca636","#fca835","#fca934","#fdab33","#fdac33","#fdae32","#fdaf31","#fdb130","#fdb22f","#fdb42f","#fdb52e","#feb72d","#feb82c","#feba2c","#febb2b","#febd2a","#febe2a","#fec029","#fdc229","#fdc328","#fdc527","#fdc627","#fdc827","#fdca26","#fdcb26","#fccd25","#fcce25","#fcd025","#fcd225","#fbd324","#fbd524","#fbd724","#fad824","#fada24","#f9dc24","#f9dd25","#f8df25","#f8e125","#f7e225","#f7e425","#f6e626","#f6e826","#f5e926","#f5eb27","#f4ed27","#f3ee27","#f3f027","#f2f227","#f1f426","#f1f525","#f0f724","#f0f921"
+    ]; 
 
     const projection = d3.geoAlbersUsa()
         .scale(1000)
@@ -22,10 +26,121 @@ function init() {
 
     const color = d3.scaleOrdinal().range(colorScheme);
 
-    let selectedState = null; // Track the selected state
+    // Assuming 'trimesters' is defined elsewhere in your code
+    const trimesters = ["2023-Q1", "2023-Q2", "2023-Q3", "2023-Q4"];
 
-    // Initialize processedTotal
-    const processedTotal = {};
+    // Define the data domains and axis for the slider
+    var yeardomain = [0, trimesters.length - 1];
+    var axisyears = trimesters.map(function(trimester) {
+        return parseFloat(trimester.substring(0, 4));
+    });
+
+    // Define data for the slider pointer
+    var pointerdata = [
+        { x: 0, y: 0 },
+        { x: 0, y: 25 },
+        { x: 25, y: 25 },
+        { x: 25, y: 0 }
+    ];
+
+    // Create a scale for the slider
+    var scale = d3.scaleLinear()
+        .domain(yeardomain)
+        .rangeRound([0, width_slider]);
+
+    // Create the slider axis
+    var x = d3.axisTop(scale)
+        .tickFormat(function(d) {
+            return trimesters[d];
+        })
+        .tickSize(0);
+
+    // Append the axis to the slider
+    svg
+        .append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + trans + "," + trans + ")")
+        .call(x);
+
+    // Create a draggable slider cursor
+    var drag = d3.drag()
+        .origin(function() {
+            return {
+                x: d3.select(this).attr("x"),
+                y: d3.select(this).attr("y")
+            };
+        })
+        .on("start", dragstart)
+        .on("drag", dragmove)
+        .on("end", dragend);
+
+    // Append elements to the slider for selecting trimesters
+    svg
+        .append("g")
+        .append("rect")
+        .attr("class", "slideraxis")
+        .attr("width", width_slider)
+        .attr("height", 7)
+        .attr("x", trans)
+        .attr("y", trans);
+
+    var cursor = svg
+        .append("g")
+        .attr("class", "move")
+        .append("svg")
+        .attr("x", width_slider + trans)
+        .attr("y", trans)
+        .attr("width", 30)
+        .attr("height", 60);
+
+    // Call the drag behavior on the cursor element
+    cursor.call(drag);
+
+    // Define a line drawing function for the cursor
+    var drawline = d3.line()
+        .x(function(d) {
+            return d.x;
+        })
+        .y(function(d) {
+            return d.y;
+        })
+        .curve(d3.curveLinear);
+
+    // Add a cursor path
+    cursor
+        .append("path")
+        .attr("class", "cursor")
+        .attr("transform", "translate(" + 7 + ",0)")
+        .attr("d", drawline(pointerdata));
+
+    // Event handlers for slider cursor
+    cursor.on("mouseover", function() {
+        d3.select(".move").style("cursor", "hand");
+    });
+
+    var aux = 0;
+
+    function drawMap(year) {
+        // Implement your map update logic here based on the selected year (aux)
+    }
+
+    function dragmove() {
+        var x = Math.max(0, Math.min(width_slider, d3.event.x));
+        d3.select(this).attr("x", x);
+        var z = Math.round(scale.invert(x));
+        aux = z;
+        drawMap(z);
+    }
+
+    function dragstart() {
+        d3.select(".cursor").style("fill", "#D9886C");
+    }
+
+    function dragend() {
+        d3.select(".cursor").style("fill", "");
+    }
+
+    // Your data processing and map drawing logic should go here
 
     Promise.all([
         d3.json("usa.json"),  // Load GeoJSON data
@@ -54,129 +169,8 @@ function init() {
             }
         });
 
-        const totalMigration = calculateTotalMigration(processedData);
-
-        // Add a total population display
-        const totalPopulationDisplay = svg.append("text")
-            .attr("class", "total-population")
-            .attr("x", 10)
-            .attr("y", 20)
-            .text("Total Migration: " + totalMigration);
-
-        g.selectAll("path")
-            .data(json.features)
-            .enter()
-            .append("path")
-            .attr("d", path)
-            .attr("class", "feature")
-            .style("fill", function (d) {
-                return color(d.properties.NAME);
-            })
-            .on("click", clicked)
-            .on("mouseover", function (d) {
-                const stateName = d.properties.NAME;
-                const data = processedData[stateName];
-                const formattedData = formatData(data);
-                d3.select(this).style("fill", "blue"); // Change color on mouseover
-                displayMigrationInfo(stateName, formattedData);
-            })
-            .on("mouseout", function () {
-                if (selectedState === null) {
-                    d3.select(this).style("fill", function (d) {
-                        return color(d.properties.NAME);
-                    }); // Restore original color on mouseout
-                    hideMigrationInfo();
-                }
-            })
-            .append("title")
-            .text(function (d) {
-                const stateName = d.properties.NAME;
-                const data = processedData[stateName];
-                const formattedData = formatData(data);
-                return stateName + "\n" + formattedData;
-            });
-
-        // Panning behavior
-        svg.call(d3.drag()
-            .subject(() => ({ x: 0, y: 0 }))
-            .on("start", started)
-            .on("drag", dragged));
-
-        function started() {
-            // Disable click event when dragging
-            svg.on(".click", null);
-        }
-
-        function dragged(event) {
-            g.attr("transform", d3.event.transform);
-        }
-
-        // Reset function
-        function reset() {
-            g.selectAll(".feature").style("fill", function (d) {
-                return color(d.properties.NAME);
-            });
-            selectedState = null; // Reset the selected state
-            g.transition().duration(750).call(zoom.transform, d3.zoomIdentity);
-        }
-
-        // Zooming behavior
-        const zoom = d3.zoom()
-            .scaleExtent([1, 8])
-            .on("zoom", zoomed);
-
-        svg.call(zoom);
-
-        function zoomed(event) {
-            g.attr("transform", event.transform);
-        }
-
-        function clicked(event, d) {
-            const [[x0, y0], [x1, y1]] = path.bounds(d);
-            event.stopPropagation();
-            reset();
-            selectedState = d; // Store the selected state
-            d3.select(this).style("fill", "red");
-            svg.transition().duration(750).call(
-                zoom.transform,
-                d3.zoomIdentity
-                    .translate(width / 2, height / 2)
-                    .scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
-                    .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
-                d3.pointer(event, svg.node())
-            );
-        }
-
-        svg.on("click", reset);
     });
 
-    function displayMigrationInfo(stateName, formattedData) {
-        d3.select(".total-population")
-            .text(stateName + "\n" + formattedData);
-    }
-
-    function hideMigrationInfo() {
-        const totalMigration = calculateTotalMigration(processedData);
-        d3.select(".total-population").text("Total Migration: " + totalMigration);
-    }
-}
-
-function formatData(data) {
-    const formattedData = [];
-    for (const category in data) {
-        formattedData.push(`${category}: ${data[category]}`);
-    }
-    return formattedData.join("\n");
-}
-
-function calculateTotalMigration(data) {
-    let total = 0;
-    for (const stateData of Object.values(data)) {
-        for (const category in stateData) {
-            total += stateData[category];
-        }
-    }
-    return total;
 }
 
 window.onload = init;
---
+++ Heat_Map/HeatMapM.js
@@ -0,0 +1,182 @@
+function init() {
+    const width = 1200;
+    const height = 1000;
+
+    const colorScheme = [
+        "#fbb4ae", "#b3cde3", "#ccebc5", "#decbe4", "#fed9a6", "#ffffcc", "#e5d8bd", "#fddaec", "#f2f2f2"
+    ];
+
+    const projection = d3.geoAlbersUsa()
+        .scale(1000)
+        .translate([width / 2, height / 2]);
+
+    const path = d3.geoPath()
+        .projection(projection);
+
+    const svg = d3.select("#chart")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    const g = svg.append("g");
+
+    const color = d3.scaleOrdinal().range(colorScheme);
+
+    let selectedState = null; // Track the selected state
+
+    // Initialize processedTotal
+    const processedTotal = {};
+
+    Promise.all([
+        d3.json("usa.json"),  // Load GeoJSON data
+        d3.csv("us_migration_data.csv")  // Load CSV data
+    ]).then(function (data) {
+        const json = data[0];
+        const csvData = data[1];
+
+        const processedData = {};
+        csvData.forEach(function (d) {
+            const stateName = d['US States'];
+            processedData[stateName] = {
+                Bangladesh: +d.Bangladesh,
+                China: +d.China,
+                India: +d.India,
+                Iran: +d.Iran,
+                Korea: +d.Korea,
+                Pakistan: +d.Pakistan,
+                Philippines: +d.Philippines,
+                Taiwan: +d.Taiwan,
+                Vietnam: +d.Vietnam,
+                Others: +d.Others,
+            };
+            processedTotal[stateName] = {
+                Total: +d.Total
+            }
+        });
+
+        const totalMigration = calculateTotalMigration(processedData);
+
+        // Add a total population display
+        const totalPopulationDisplay = svg.append("text")
+            .attr("class", "total-population")
+            .attr("x", 10)
+            .attr("y", 20)
+            .text("Total Migration: " + totalMigration);
+
+        g.selectAll("path")
+            .data(json.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr("class", "feature")
+            .style("fill", function (d) {
+                return color(d.properties.NAME);
+            })
+            .on("click", clicked)
+            .on("mouseover", function (d) {
+                const stateName = d.properties.NAME;
+                const data = processedData[stateName];
+                const formattedData = formatData(data);
+                d3.select(this).style("fill", "blue"); // Change color on mouseover
+                displayMigrationInfo(stateName, formattedData);
+            })
+            .on("mouseout", function () {
+                if (selectedState === null) {
+                    d3.select(this).style("fill", function (d) {
+                        return color(d.properties.NAME);
+                    }); // Restore original color on mouseout
+                    hideMigrationInfo();
+                }
+            })
+            .append("title")
+            .text(function (d) {
+                const stateName = d.properties.NAME;
+                const data = processedData[stateName];
+                const formattedData = formatData(data);
+                return stateName + "\n" + formattedData;
+            });
+
+        // Panning behavior
+        svg.call(d3.drag()
+            .subject(() => ({ x: 0, y: 0 }))
+            .on("start", started)
+            .on("drag", dragged));
+
+        function started() {
+            // Disable click event when dragging
+            svg.on(".click", null);
+        }
+
+        function dragged(event) {
+            g.attr("transform", d3.event.transform);
+        }
+
+        // Reset function
+        function reset() {
+            g.selectAll(".feature").style("fill", function (d) {
+                return color(d.properties.NAME);
+            });
+            selectedState = null; // Reset the selected state
+            g.transition().duration(750).call(zoom.transform, d3.zoomIdentity);
+        }
+
+        // Zooming behavior
+        const zoom = d3.zoom()
+            .scaleExtent([1, 8])
+            .on("zoom", zoomed);
+
+        svg.call(zoom);
+
+        function zoomed(event) {
+            g.attr("transform", event.transform);
+        }
+
+        function clicked(event, d) {
+            const [[x0, y0], [x1, y1]] = path.bounds(d);
+            event.stopPropagation();
+            reset();
+            selectedState = d; // Store the selected state
+            d3.select(this).style("fill", "red");
+            svg.transition().duration(750).call(
+                zoom.transform,
+                d3.zoomIdentity
+                    .translate(width / 2, height / 2)
+                    .scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
+                    .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
+                d3.pointer(event, svg.node())
+            );
+        }
+
+        svg.on("click", reset);
+    });
+
+    function displayMigrationInfo(stateName, formattedData) {
+        d3.select(".total-population")
+            .text(stateName + "\n" + formattedData);
+    }
+
+    function hideMigrationInfo() {
+        const totalMigration = calculateTotalMigration(processedData);
+        d3.select(".total-population").text("Total Migration: " + totalMigration);
+    }
+}
+
+function formatData(data) {
+    const formattedData = [];
+    for (const category in data) {
+        formattedData.push(`${category}: ${data[category]}`);
+    }
+    return formattedData.join("\n");
+}
+
+function calculateTotalMigration(data) {
+    let total = 0;
+    for (const stateData of Object.values(data)) {
+        for (const category in stateData) {
+            total += stateData[category];
+        }
+    }
+    return total;
+}
+
+window.onload = init;
