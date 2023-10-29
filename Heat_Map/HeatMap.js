5885a33cd0a11010030dd98c6d736c33efe279bd
OowanoO <zhenyaowan@gmail.com>
2023-10-29 12:57:19 +0800

    c

--- Heat_Map/HeatMap.js
+++ Heat_Map/HeatMap.js
@@ -1,4 +1,5 @@
-function init() {
+document.addEventListener('DOMContentLoaded', function () {
+    // Define the dimensions of the SVG canvas.
     const width = 1200;
     const height = 1000;
 
@@ -8,50 +9,70 @@ function init() {
         .attr('width', width)
         .attr('height', height);
 
-    // Define your projection and path.
-    const projection = d3.geoAlbersUsa();
-    const path = d3.geoPath().projection(projection);
-
     // Load the US GeoJSON data.
     d3.json('usa.json').then(function (usData) {
-        // Add an initial projection of the USA.
-        projection.fitSize([width, height], usData);
+        // Load the US state data from the CSV.
+        d3.csv('us_migration_data.csv').then(function (data) {
+            const csvData = data.slice(1); // Skip the first object (contains column names)
 
-        // Load the US migration data from CSV.
-        d3.csv('us_migration_data.csv').then(function (migrationData) {
-            // Create a dictionary to map state names to their migration total.
+            // Create a dictionary to map state names to their data.
             const processedData = {};
-            migrationData.forEach(function (d) {
-                processedData[d['US States']] = +d.Total;
+            csvData.forEach(function (d) {
+                const stateName = d['US States'];
+                processedData[stateName] = {
+                    Bangladesh: +d.Bangladesh,
+                    China: +d.China,
+                    India: +d.India,
+                    Iran: +d.Iran,
+                    Korea: +d.Korea,
+                    Pakistan: +d.Pakistan,
+                    Philippines: +d.Philippines,
+                    Taiwan: +d.Taiwan,
+                    Vietnam: +d.Vietnam,
+                    Others: +d.Others,
+                    Total: +d.Total
+                };
             });
 
-            // Draw the map.
+            // Create a GeoProjection and path.
+            const projection = d3.geoAlbersUsa();
+            const path = d3.geoPath().projection(projection);
+
+            // Define a scale for the circle size based on the Total migration.
+            const circleScale = d3.scaleSqrt()
+                .domain([0, d3.max(csvData, d => +d.Total)]) // Adjust the domain as needed
+                .range([0, 20]); // Adjust the range for the circle size
+
+            // Bind the GeoJSON data to the SVG and draw the map.
             svg.selectAll('path')
                 .data(usData.features)
                 .enter()
                 .append('path')
                 .attr('d', path)
-                .attr('fill', 'none') // Set the fill to 'none' for the map
+                .attr('fill', 'steelblue')
                 .attr('stroke', 'white')
-                .attr('stroke-width', 1);
+                .on('mouseover', function (event, d) {
+                    // Get the state name and total migration data.
+                    const stateName = d.properties.name;
+                    const totalMigration = processedData[stateName].Total;
 
-            // Draw circle dots based on migration total.
-            svg.selectAll('circle')
-                .data(usData.features)
-                .enter()
-                .append('circle')
-                .attr('cx', function (d) {
-                    return projection(path.centroid(d)[0]);
+                    // Calculate the circle position using the projection.
+                    const [x, y] = path.centroid(d);
+
+                    // Append a black circle with a transition for the pop-out effect.
+                    svg.append('circle')
+                        .attr('cx', x)
+                        .attr('cy', y)
+                        .attr('r', 0) // Start with a radius of 0
+                        .attr('fill', 'black')
+                        .transition()
+                        .duration(500) // Adjust the duration as needed
+                        .attr('r', circleScale(totalMigration)); // Scale the circle based on total migration
                 })
-                .attr('cy', function (d) {
-                    return projection(path.centroid(d)[1]);
-                })
-                .attr('r', function (d) {
-                    return processedData[d.properties.name] || 0;
-                })
-                .attr('fill', 'blue');
+                .on('mouseout', function () {
+                    // Remove the circle on mouseout.
+                    svg.select('circle').remove();
+                });
         });
     });
-}
-
-window.onload = init;
+});
