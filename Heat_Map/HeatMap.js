d3a76ec68915eee7421ec147e337d7011ac7a046
OowanoO <zhenyaowan@gmail.com>
2023-10-29 13:15:06 +0800

    c

--- Heat_Map/HeatMap.js
+++ Heat_Map/HeatMap.js
@@ -1,78 +1,219 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Define the dimensions of the SVG canvas.
+function init() {
     const width = 1200;
     const height = 1000;
 
-    // Append an SVG element to the body of the page.
-    const svg = d3.select('#chart')
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height);
+    const colorScheme = [
+        "#fbb4ae", "#b3cde3", "#ccebc5", "#decbe4", "#fed9a6", "#ffffcc", "#e5d8bd", "#fddaec", "#f2f2f2"
+    ];
 
-    // Load the US GeoJSON data.
-    d3.json('usa.json').then(function (usData) {
-        // Load the US state data from the CSV.
-        d3.csv('us_migration_data.csv').then(function (data) {
-            const csvData = data.slice(1); // Skip the first object (contains column names)
+    const projection = d3.geoAlbersUsa()
+        .scale(1000)
+        .translate([width / 2, height / 2]);
 
-            // Create a dictionary to map state names to their data.
-            const processedData = {};
-            csvData.forEach(function (d) {
-                const stateName = d['US States'];
-                processedData[stateName] = {
-                    Bangladesh: +d.Bangladesh,
-                    China: +d.China,
-                    India: +d.India,
-                    Iran: +d.Iran,
-                    Korea: +d.Korea,
-                    Pakistan: +d.Pakistan,
-                    Philippines: +d.Philippines,
-                    Taiwan: +d.Taiwan,
-                    Vietnam: +d.Vietnam,
-                    Others: +d.Others,
-                    Total: +d.Total
-                };
+    const path = d3.geoPath()
+        .projection(projection);
+
+    const svg = d3.select("#chart")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    const g = svg.append("g");
+
+    const color = d3.scaleOrdinal().range(colorScheme);
+
+    let selectedState = null; // Track the selected state
+
+    // Initialize processedTotal
+    const processedTotal = {};
+
+    Promise.all([
+        d3.json("usa.json"),  // Load GeoJSON data
+        d3.csv("us_migration_data.csv")  // Load CSV data
+    ]).then(function (data) {
+        const json = data[0];
+        const csvData = data[1];
+
+        const processedData = {};
+        csvData.forEach(function (d) {
+            const stateName = d['US States'];
+            processedData[stateName] = {
+                Bangladesh: +d.Bangladesh,
+                China: +d.China,
+                India: +d.India,
+                Iran: +d.Iran,
+                Korea: +d.Korea,
+                Pakistan: +d.Pakistan,
+                Philippines: +d.Philippines,
+                Taiwan: +d.Taiwan,
+                Vietnam: +d.Vietnam,
+                Others: +d.Others,
+            };
+            processedTotal[stateName] = {
+                Total: +d.Total
+            }
+        });
+
+        const totalMigration = calculateTotalMigration(processedData);
+
+        // Add a total population display
+        const totalPopulationDisplay = svg.append("text")
+            .attr("class", "total-population")
+            .attr("x", 10)
+            .attr("y", 20)
+            .text("Total Migration: " + totalMigration);
+
+        g.selectAll("path")
+            .data(json.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr("class", "feature")
+            .style("fill", function (d) {
+                return color(d.properties.NAME);
+            })
+            .on("click", clicked)
+            .on("mouseover", function (d) {
+                const stateName = d.properties.NAME;
+                const data = processedData[stateName];
+                const formattedData = formatData(data);
+                d3.select(this).style("fill", "blue"); // Change color on mouseover
+                displayMigrationInfo(stateName, formattedData);
+            })
+            .on("mouseout", function () {
+                if (selectedState === null) {
+                    d3.select(this).style("fill", function (d) {
+                        return color(d.properties.NAME);
+                    }); // Restore original color on mouseout
+                    hideMigrationInfo();
+                }
+            })
+            .append("title")
+            .text(function (d) {
+                const stateName = d.properties.NAME;
+                const data = processedData[stateName];
+                const formattedData = formatData(data);
+                return stateName + "\n" + formattedData;
             });
 
-            // Create a GeoProjection and path.
-            const projection = d3.geoAlbersUsa();
-            const path = d3.geoPath().projection(projection);
+        // Panning behavior
+        svg.call(d3.drag()
+            .subject(() => ({ x: 0, y: 0 }))
+            .on("start", started)
+            .on("drag", dragged));
 
-            // Define a scale for the circle size based on the Total migration.
-            const circleScale = d3.scaleSqrt()
-                .domain([0, d3.max(csvData, d => +d.Total)]) // Adjust the domain as needed
-                .range([0, 20]); // Adjust the range for the circle size
+        function started() {
+            // Disable click event when dragging
+            svg.on(".click", null);
+        }
 
-            // Bind the GeoJSON data to the SVG and draw the map.
-            svg.selectAll('path')
-                .data(usData.features)
-                .enter()
-                .append('path')
-                .attr('d', path)
-                .attr('fill', 'steelblue')
-                .attr('stroke', 'white')
-                .on('mouseover', function (event, d) {
-                    // Get the state name and total migration data if available.
-                    const stateName = d.properties.name;
-                    const totalMigration = processedData[stateName] ? processedData[stateName].Total : 0; // Default to 0 if state not found
+        function dragged(event) {
+            g.attr("transform", d3.event.transform);
+        }
 
-                    // Calculate the circle position using the projection.
-                    const [x, y] = path.centroid(d);
+        // Reset function
+        function reset() {
+            g.selectAll(".feature").style("fill", function (d) {
+                return color(d.properties.NAME);
+            });
+            selectedState = null; // Reset the selected state
+            g.transition().duration(750).call(zoom.transform, d3.zoomIdentity);
+        }
 
-                    // Append a black circle with a transition for the pop-out effect.
-                    svg.append('circle')
-                        .attr('cx', x)
-                        .attr('cy', y)
-                        .attr('r', 0) // Start with a radius of 0
-                        .attr('fill', 'black')
-                        .transition()
-                        .duration(500) // Adjust the duration as needed
-                        .attr('r', circleScale(totalMigration)); // Scale the circle based on total migration
-                })
-                .on('mouseout', function () {
-                    // Remove the circle on mouseout.
-                    svg.select('circle').remove();
-                });
-        });
+        // Zooming behavior
+        const zoom = d3.zoom()
+            .scaleExtent([1, 8])
+            .on("zoom", zoomed);
+
+        svg.call(zoom);
+
+        function zoomed(event) {
+            g.attr("transform", event.transform);
+        }
+
+        function clicked(event, d) {
+            const [[x0, y0], [x1, y1]] = path.bounds(d);
+            event.stopPropagation();
+            reset();
+            selectedState = d; // Store the selected state
+            d3.select(this).style("fill", "red");
+            svg.transition().duration(750).call(
+                zoom.transform,
+                d3.zoomIdentity
+                    .translate(width / 2, height / 2)
+                    .scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
+                    .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
+                d3.pointer(event, svg.node())
+            );
+        }
+
+        svg.on("click", reset);
+
+        // Add animated bubbles
+        const states = json.features.map(d => d.properties.NAME);
+        animateBubbles(states, processedData);
     });
-});
+
+    function displayMigrationInfo(stateName, formattedData) {
+        d3.select(".total-population")
+            .text(stateName + "\n" + formattedData);
+    }
+
+    function hideMigrationInfo() {
+        const totalMigration = calculateTotalMigration(processedData);
+        d3.select(".total-population").text("Total Migration: " + totalMigration);
+    }
+}
+
+function formatData(data) {
+    const formattedData = [];
+    for (const category in data) {
+        formattedData.push(`${category}: ${data[category]}`);
+    }
+    return formattedData.join("\n");
+}
+
+function calculateTotalMigration(data) {
+    let total = 0;
+    for (const stateData of Object.values(data)) {
+        for (const category in stateData) {
+            total += stateData[category];
+        }
+    }
+    return total;
+}
+
+function animateBubbles(states, data) {
+    const bubbleG = svg.append("g").classed("animated-bubbles", true);
+    const delay = 500; // Adjust the delay as needed
+    const duration = 1000; // Adjust the duration as needed
+
+    let index = 0;
+    function drawNextBubble() {
+        if (index >= states.length) {
+            return; // All states processed
+        }
+
+        const stateName = states[index];
+        const totalMigration = data[stateName].Total;
+        const [x, y] = projection([0, 0]); // Replace [0, 0] with the coordinates you want to place the bubble
+
+        bubbleG.append("circle")
+            .attr("cx", x)
+            .attr("cy", y)
+            .attr("r", 0)
+            .attr("fill", "black")
+            .transition()
+            .duration(duration)
+            .attr("r", totalMigration) // Adjust bubble size based on total migration
+            .on("end", () => {
+                // Move to the next state
+                index++;
+                setTimeout(drawNextBubble, delay);
+            });
+    }
+
+    drawNextBubble(); // Start the animation
+}
+
+window.onload = init;
