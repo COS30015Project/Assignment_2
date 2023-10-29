6094b7b5bc9b1db393722fa5412a14673f4da23d
OowanoO <zhenyaowan@gmail.com>
2023-10-29 10:48:16 +0800

    c

--- Heat_Map/HeatMap.js
+++ Heat_Map/HeatMap.js
@@ -50,22 +50,21 @@ function init() {
             .attr('fill', 'steelblue')
             .attr('stroke', 'white')
             .on('mouseover', function (d) {
-                const stateData = d.properties.NAME;
-                const data = processedData[stateName];
-                const formattedData = formatData(data);
+                const stateName = d.properties.NAME; // Use the correct property name
+                const stateData = processedData[stateName];
 
                 div.style('opacity', 0.9);
-                div.html('<b>' + d.properties.NAME + '</b><br>' +
-                    'Bangladesh: ' + formattedData + '<br>' +
-                    'China: ' + formattedData + '<br>' +
-                    'India: ' + formattedData + '<br>' +
-                    'Iran: ' + formattedData + '<br>' +
-                    'Korea: ' + formattedData + '<br>' +
-                    'Pakistan: ' + formattedData + '<br>' +
-                    'Philippines: ' + formattedData + '<br>' +
-                    'Taiwan: ' + formattedData + '<br>' +
-                    'Vietnam: ' + formattedData + '<br>' +
-                    'Others: ' + formattedData + '<br>' +
+                div.html('<b>' + stateName + '</b><br>' +
+                    'Bangladesh: ' + stateData.Bangladesh + '<br>' +
+                    'China: ' + stateData.China + '<br>' +
+                    'India: ' + stateData.India + '<br>' +
+                    'Iran: ' + stateData.Iran + '<br>' +
+                    'Korea: ' + stateData.Korea + '<br>' +
+                    'Pakistan: ' + stateData.Pakistan + '<br>' +
+                    'Philippines: ' + stateData.Philippines + '<br>' +
+                    'Taiwan: ' + stateData.Taiwan + '<br>' +
+                    'Vietnam: ' + stateData.Vietnam + '<br>' +
+                    'Others: ' + stateData.Others + '<br>' +
                     'Total: ' + stateData.Total)
                     .style('left', (d3.event.pageX + 23) + 'px')
                     .style('top', (d3.event.pageY - 20) + 'px');
