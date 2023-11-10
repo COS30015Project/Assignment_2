62e87fff1f97d4b2d7d0cec12cf4da7abe73d244
OowanoO <zhenyaowan@gmail.com>
2023-11-10 21:13:35 +0800

    commit

--- Bar_Chart/BarChart.js
+++ Bar_Chart/BarChart.js
@@ -1,103 +1,96 @@
 function init()
 {
-    // Use 'fetch' to load data from a CSV file
-fetch('BarChartDataset.csv')
-.then(response => response.text())
-.then(data => {
-    const parsedData = d3.csvParse(data);
-    visualizeData(parsedData);
-})
-.catch(error => console.error('Error loading data:', error));
+    d3.csv("BarChart.csv")
+    .then(function (data) {
+        visualizeData(data);
+    })
+    .catch(function (error) {
+        console.error("Error loading data:", error);
+    });
 
 function visualizeData(data) {
-// Extract US state names from the dataset
-const usStates = data.map(d => d['US States']);
+    // Extract country names and corresponding total values
+    const countries = data.map(d => d['US States']);
+    const totalValues = data.map(d => +d['Total']);
 
-// Extract country names from the headers
-const countries = data.columns.slice(1);
+    // Define the dimensions for the SVG
+    const margin = { top: 20, right: 20, bottom: 60, left: 80 };
+    const width = 800 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
 
-// Create an array to store the total values for each country
-const totalValues = countries.map(country => ({
-    country,
-    total: d3.sum(data, d => +d[country])
-}));
+    // Create an SVG element
+    const svg = d3.select("#chart")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
 
-// Define color scale
-const colorScale = d3.scaleOrdinal()
-    .domain(usStates)
-    .range(d3.schemeCategory10);
+    // Create color scale based on US states
+    const colorScale = d3.scaleOrdinal()
+        .domain(data.columns.slice(1)) // US states
+        .range(d3.schemeCategory10);
 
-// Define dimensions for the SVG
-const margin = { top: 30, right: 30, bottom: 80, left: 60 };
-const width = 800 - margin.left - margin.right;
-const height = 560 - margin.top - margin.bottom;
+    // Create x and y scales
+    const x = d3.scaleLinear()
+        .domain([0, d3.max(totalValues)])
+        .range([0, width]);
 
-// Create SVG element
-const svg = d3.select("#chart")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+    const y = d3.scaleBand()
+        .domain(countries)
+        .range([0, height])
+        .padding(0.1);
 
-// Define x and y scales
-const x = d3.scaleBand()
-    .domain(countries)
-    .range([0, width])
-    .padding(0.1);
+    // Create bars
+    svg.selectAll(".bar-group")
+        .data(data)
+        .enter()
+        .append("g")
+        .attr("class", "bar-group")
+        .attr("transform", d => `translate(0, ${y(d['US States'])})`)
+        .selectAll(".bar")
+        .data(d => data.columns.slice(1).map(state => ({ state, value: +d[state] })))
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", 0)
+        .attr("y", d => y(d.state))
+        .attr("width", d => x(d.value))
+        .attr("height", y.bandwidth())
+        .attr("fill", d => colorScale(d.state));
 
-const y = d3.scaleLinear()
-    .domain([0, d3.max(totalValues, d => d.total)])
-    .range([height, 0]);
+    // Add axes
+    svg.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(x));
 
-// Create bars
-svg.selectAll(".bar-group")
-    .data(totalValues)
-    .enter()
-    .append("rect")
-    .attr("class", "bar")
-    .attr("x", d => x(d.country))
-    .attr("y", d => y(d.total))
-    .attr("width", x.bandwidth())
-    .attr("height", d => height - y(d.total))
-    .attr("fill", (d, i) => colorScale(usStates[i]));
+    svg.append("g")
+        .attr("class", "y-axis")
+        .call(d3.axisLeft(y));
 
-// Add axes
-svg.append("g")
-    .attr("class", "x-axis")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-    .attr("transform", "rotate(-45)")
-    .style("text-anchor", "end");
+    // Add legend
+    const legend = svg.append("g")
+        .attr("class", "legend")
+        .attr("transform", `translate(${width + 10}, 0)`);
 
-svg.append("g")
-    .attr("class", "y-axis")
-    .call(d3.axisLeft(y));
+    const legendItems = legend.selectAll(".legend-item")
+        .data(data.columns.slice(1))
+        .enter()
+        .append("g")
+        .attr("class", "legend-item")
+        .attr("transform", (d, i) => `translate(0, ${i * 20})`);
 
-// Add legend
-const legend = svg.append("g")
-    .attr("class", "legend")
-    .attr("transform", `translate(${width + 20}, 0)`);
+    legendItems.append("rect")
+        .attr("width", 15)
+        .attr("height", 15)
+        .attr("fill", colorScale);
 
-const legendItems = legend.selectAll(".legend-item")
-    .data(usStates)
-    .enter()
-    .append("g")
-    .attr("class", "legend-item")
-    .attr("transform", (d, i) => `translate(0, ${i * 20})`);
-
-legendItems.append("rect")
-    .attr("width", 15)
-    .attr("height", 15)
-    .attr("fill", d => colorScale(d));
-
-legendItems.append("text")
-    .text(d => d)
-    .attr("x", 20)
-    .attr("y", 10);
+    legendItems.append("text")
+        .text(d => d)
+        .attr("x", 20)
+        .attr("y", 10);
 }
-
 }
 
 window.onload = init;
