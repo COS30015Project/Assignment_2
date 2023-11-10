616596b8f898191191021feb08c4b13a3de81c00
OowanoO <zhenyaowan@gmail.com>
2023-11-10 21:28:37 +0800

    commit

--- Bar_Chart/BarChart.js
+++ Bar_Chart/BarChart.js
@@ -1,99 +1,101 @@
 function init() {
-    let data; // Define a global variable to hold the data
-
     // Load the data from the CSV file
-    d3.csv("BarChartDataset.csv")
-        .then(function (csvData) {
-            data = csvData; // Store the loaded data in the global variable
-            visualizeData(data);
-        })
-        .catch(function (error) {
-            console.error("Error loading data:", error);
-        });
+d3.csv("your_dataset.csv")
+.then(function (data) {
+    visualizeData(data);
+})
+.catch(function (error) {
+    console.error("Error loading data:", error);
+});
 
-    // Function to visualize the data
-    function visualizeData(data) {
-        // Extract country names from the dataset
-        const countries = data.map(d => d['US States']);
+// Function to visualize the data
+function visualizeData(data) {
+// Extract country names and their corresponding total values
+const countries = data.columns.slice(1);
+const totals = data[data.length - 1];
 
-        // Define color scale for each US state within a country
-        const colorScale = d3.scaleOrdinal()
-            .domain(data.columns.slice(1))
-            .range(d3.schemeCategory10);
+// Combine country names and total values into an array of objects
+const countryData = countries.map((country, index) => ({
+    country: country,
+    total: +totals[country], // Convert total to a number
+    states: data.map(d => ({ state: d['US States'], value: +d[country] })) // Extract state values
+}));
 
-        // Define the dimensions and margins for the SVG
-        const margin = { top: 30, right: 30, bottom: 80, left: 60 };
-        const width = 800 - margin.left - margin.right;
-        const height = 560 - margin.top - margin.bottom;
+// Sort the data by total value in descending order
+countryData.sort((a, b) => b.total - a.total);
 
-        // Create an SVG element
-        const svg = d3.select("#chart")
-            .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", `translate(${margin.left},${margin.top})`);
+// Define the dimensions and margins for the SVG
+const margin = { top: 20, right: 20, bottom: 30, left: 80 };
+const width = 800 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
 
-        // Define x and y scales
-        const x = d3.scaleLinear()
-            .domain([0, d3.max(data, d => +d['Total'])]) // Assuming 'Total' is the column for total value
-            .range([0, width]);
+// Create an SVG element
+const svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
-        const y = d3.scaleBand()
-            .domain(countries)
-            .range([0, height])
-            .padding(0.1);
+// Define x and y scales
+const x = d3.scaleLinear()
+    .domain([0, d3.max(countryData, d => d.total)])
+    .range([0, width]);
 
-        // Create bars
-        svg.selectAll(".bar-group")
-            .data(data)
-            .enter()
-            .append("g")
-            .attr("class", "bar-group")
-            .attr("transform", d => `translate(0, ${y(d['US States'])})`)
-            .selectAll("rect")
-            .data(d => data.columns.slice(1).map(key => ({ key, value: +d[key] })))
-            .enter()
-            .append("rect")
-            .attr("class", "bar")
-            .attr("x", 0)
-            .attr("y", d => y(d.key))
-            .attr("width", d => x(d.value))
-            .attr("height", y.bandwidth())
-            .attr("fill", d => colorScale(d.key));
+const y = d3.scaleBand()
+    .domain(countryData.map(d => d.country))
+    .range([height, 0])
+    .padding(0.1);
 
-        // Add axes
-        svg.append("g")
-            .attr("class", "x-axis")
-            .attr("transform", `translate(0, ${height})`)
-            .call(d3.axisBottom(x).ticks(10).tickFormat(d3.format(".2s"))); // Format x-axis labels as SI units
+// Create bars
+svg.selectAll(".bar")
+    .data(countryData)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", 0)
+    .attr("y", d => y(d.country))
+    .attr("width", d => x(d.total))
+    .attr("height", y.bandwidth())
+    .on("mouseover", function (event, d) {
+        // Show tooltip on mouseover
+        d3.select(this).style("fill", "darkblue");
+        showTooltip(d);
+    })
+    .on("mouseout", function (event, d) {
+        // Remove tooltip on mouseout
+        d3.select(this).style("fill", "steelblue");
+        hideTooltip();
+    });
 
-        svg.append("g")
-            .attr("class", "y-axis")
-            .call(d3.axisLeft(y));
+// Add axes
+svg.append("g")
+    .attr("class", "x-axis")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x));
 
-        // Add legend
-        const legend = svg.append("g")
-            .attr("class", "legend")
-            .attr("transform", `translate(${width + 20}, 0)`);
+svg.append("g")
+    .attr("class", "y-axis")
+    .call(d3.axisLeft(y));
 
-        const legendItems = legend.selectAll(".legend-item")
-            .data(data.columns.slice(1))
-            .enter()
-            .append("g")
-            .attr("class", "legend-item")
-            .attr("transform", (d, i) => `translate(0, ${i * 20})`);
+// Function to show tooltip
+function showTooltip(data) {
+    const tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("position", "absolute")
+        .style("background-color", "white")
+        .style("padding", "8px")
+        .html(`Country: ${data.country}<br>Total: ${data.total.toLocaleString()}`);
 
-        legendItems.append("rect")
-            .attr("width", 15)
-            .attr("height", 15)
-            .attr("fill", colorScale);
+    tooltip.style("left", (event.pageX + 10) + "px")
+        .style("top", (event.pageY - 30) + "px");
+}
 
-        legendItems.append("text")
-            .text(d => d)
-            .attr("x", 20)
-            .attr("y", 10);
-    }
+// Function to hide tooltip
+function hideTooltip() {
+    d3.select(".tooltip").remove();
+}
+}
 }
 
 window.onload = init;
