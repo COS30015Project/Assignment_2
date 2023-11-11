200c1266d1d3367d03a58eff9e9641d2a13cc414
OowanoO <zhenyaowan@gmail.com>
2023-11-11 17:14:12 +0800

    c

--- Bar_Chart/BarChart.js
+++ Bar_Chart/BarChart.js
@@ -43,11 +43,11 @@ function init() {
         .enter()
         .append('rect')
         .attr('class', 'bar')
-        .attr('x', 0) // Bar starts from the left edge
+        .attr('x', d => (d.gender === 'Male') ? 0 : xScale(d.value)) // Adjust x position
         .attr('y', d => yScale(countries[d.index]))
-        .attr('width', d => xScale(d.value))
+        .attr('width', d => (d.gender === 'Male') ? xScale(d.value) : xScale(0)) // Adjust width
         .attr('height', yScale.bandwidth())
-        .attr('fill', d => colorScale(d.gender)); // Use color scale here
+        .attr('fill', d => colorScale(d.gender));
   
       // Create X axis
       svg.append('g')
