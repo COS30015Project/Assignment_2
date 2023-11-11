6f1a3a094268d834d5b1640ec4a411626e3510d4
OowanoO <zhenyaowan@gmail.com>
2023-11-11 17:32:01 +0800

    s

--- Bar_Chart/BarChart.js
+++ Bar_Chart/BarChart.js
@@ -12,10 +12,10 @@ function init() {
       // Set up chart dimensions
       const margin = { top: 20, right: 20, bottom: 30, left: 40 };
       const width = 600 - margin.left - margin.right;
-      const height = 400 - margin.top - margin.bottom;
+      const height = 300 - margin.top - margin.bottom;
   
-      // Create SVG container
-      const svg = d3.select('#chart')
+      // Create an SVG element and append a 'g' element
+      const svg = d3.select('body')
         .append('svg')
         .attr('width', width + margin.left + margin.right)
         .attr('height', height + margin.top + margin.bottom)
@@ -23,14 +23,14 @@ function init() {
         .attr('transform', `translate(${margin.left},${margin.top})`);
   
       // Create scales
-      const xScale = d3.scaleBand()
-        .domain(countries)
-        .range([0, width])
-        .padding(0.2); // Adjust the padding for the gap between bars
-  
-      const yScale = d3.scaleLinear()
+      const xScale = d3.scaleLinear()
         .domain([0, d3.max(values, d => d.value)])
-        .range([height, 0]);
+        .range([0, width]);
+  
+      const yScale = d3.scaleBand()
+        .domain(countries)
+        .range([0, height])
+        .padding(0.1);
   
       // Create color scale
       const colorScale = d3.scaleOrdinal()
@@ -43,22 +43,21 @@ function init() {
         .enter()
         .append('rect')
         .attr('class', 'bar')
-        .attr('x', d => xScale(countries[d.index]) + xScale.bandwidth() / 2 * (d.gender === 'Male' ? -1 : 1))
-        .attr('y', d => yScale(d.value))
-        .attr('width', xScale.bandwidth() / 2) // Adjust width
-        .attr('height', d => height - yScale(d.value))
+        .attr('x', d => (d.gender === 'Male') ? 0 : xScale(d.value)) // Adjust x position
+        .attr('y', d => yScale(countries[d.index]))
+        .attr('width', d => (d.gender === 'Male') ? xScale(d.value) : xScale(d.value + maleValues[d.index].value)) // Adjust width
+        .attr('height', yScale.bandwidth())
         .attr('fill', d => colorScale(d.gender));
   
       // Create X axis
       svg.append('g')
-        .attr('transform', `translate(0,${height})`)
-        .call(d3.axisBottom(xScale));
+        .call(d3.axisBottom(xScale))
+        .attr('transform', `translate(0,${height})`);
   
       // Create Y axis
       svg.append('g')
         .call(d3.axisLeft(yScale));
-  
     });
-  }
+}
   
   window.onload = init;
