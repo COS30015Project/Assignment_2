5d24c67801dd32c359842527ee91eb1a4ec7a298
OowanoO <zhenyaowan@gmail.com>
2023-11-11 17:49:30 +0800

    s

--- Bar_Chart/BarChart.js
+++ Bar_Chart/BarChart.js
@@ -1,63 +1,77 @@
-function init() { 
-    // Read the CSV file and create the chart 
-    d3.csv('BarChartDataset.csv').then(data => { 
-        // Extract data 
-        const countries = data.map(d => d['Country Name']); 
-        const maleValues = data.map(d => +d['Male']).map((d, i) => ({ value: d, gender: 'Male', index: i })); 
-        const femaleValues = data.map(d => +d['Female']).map((d, i) => ({ value: d, gender: 'Female', index: i }));
+function init() {
+    d3.csv('BarChartDataset.csv').then(data => {
+        const keys = ["Male", "Female"];
 
-      // Combine the male and female values into a single array
-      const values = maleValues.concat(femaleValues);
-    
-      // Set up chart dimensions
-      const margin = { top: 20, right: 20, bottom: 30, left: 40 };
-      const width = 600 - margin.left - margin.right;
-      const height = 300 - margin.top - margin.bottom;
-    
-      // Create an SVG element and append a 'g' element
-      const svg = d3.select('body')
-        .append('svg')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-        .append('g')
-        .attr('transform', `translate(${margin.left},${margin.top})`);
-    
-      // Create scales
-      const xScale = d3.scaleLinear()
-        .domain([0, d3.max(values, d => d.value)])
-        .range([0, width]);
-    
-      const yScale = d3.scaleBand()
-        .domain(countries)
-        .range([0, height])
-        .padding(0.1);
-    
-      // Create color scale
-      const colorScale = d3.scaleOrdinal()
-        .domain(['Male', 'Female'])
-        .range(['blue', 'pink']); // You can customize colors as needed
-    
-      // Create bars
-      svg.selectAll('.bar')
-        .data(values)
-        .enter()
-        .append('rect')
-        .attr('class', 'bar')
-        .attr('x', d => (d.gender === 'Male') ? 0 : xScale(d.value)) // Adjust x position
-        .attr('y', d => yScale(countries[d.index]))
-        .attr('width', d => (d.gender === 'Male') ? xScale(d.value) : xScale(d.value + maleValues[d.index].value)) // Adjust width
-        .attr('height', yScale.bandwidth())
-        .attr('fill', d => colorScale(d.gender));
-    
-      // Create X axis
-      svg.append('g')
-        .call(d3.axisBottom(xScale))
-        .attr('transform', `translate(0,${height})`);
-    
-      // Create Y axis
-      svg.append('g')
-        .call(d3.axisLeft(yScale));
+        const stack = d3.stack().keys(keys);
+
+        const series = stack(data);
+
+        const width = 600;
+        const height = 400;
+        const padding = 40;
+
+        const xScale = d3.scaleBand()
+            .domain(data.map(d => d['Country Name']))
+            .range([padding, width - padding])
+            .padding(0.1);
+
+        const yScale = d3.scaleLinear()
+            .domain([0, d3.max(series, d => d3.max(d, d => d[1]))])
+            .range([height - padding, padding]);
+
+        const colorScale = d3.scaleOrdinal()
+            .domain(keys)
+            .range(['blue', 'pink']);
+
+        const svg = d3.select("#chart")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+        const groups = svg.selectAll("g")
+            .data(series)
+            .enter()
+            .append("g")
+            .style("fill", (d, i) => colorScale(i));
+
+        const rects = groups.selectAll("rect")
+            .data(d => d)
+            .enter()
+            .append("rect")
+            .attr("x", (d, i) => xScale(data[i]['Country Name']))
+            .attr("y", d => yScale(d[1]))
+            .attr("height", d => yScale(d[0]) - yScale(d[1]))
+            .attr("width", xScale.bandwidth())
+            .on("mouseover", showTooltip)
+            .on("mouseout", hideTooltip);
+
+        svg.append("g")
+            .attr("transform", `translate(0, ${height - padding})`)
+            .call(d3.axisBottom(xScale));
+
+        svg.append("g")
+            .attr("transform", `translate(${padding}, 0)`)
+            .call(d3.axisLeft(yScale));
+
+        const tooltip = d3.select("body").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
+
+        function showTooltip(event, d) {
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", 0.9);
+            tooltip.html(`${d.data['Country Name']}<br>${d3.format(",")(d[1] - d[0])}`)
+                .style("left", (event.pageX) + "px")
+                .style("top", (event.pageY - 28) + "px");
+        }
+
+        function hideTooltip() {
+            tooltip.transition()
+                .duration(500)
+                .style("opacity", 0);
+        }
     });
-    }
-    
-    window.onload = init; 
+}
+
+window.onload = init;
