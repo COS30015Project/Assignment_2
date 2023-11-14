d3516176be7f9dfa9773c823f896f617305a157d
OowanoO <zhenyaowan@gmail.com>
2023-11-14 19:08:03 +0800

    commit

--- Bar_Chart/BarChart.js
+++ Bar_Chart/BarChart.js
@@ -1,121 +1,118 @@
 // D3.js code
 function init() {
-    d3.csv('BarChartDataset.csv').then(data => {
-        // Initial gender selection
-        let selectedGender = 'Total';
-
-        const keys = ["Male", "Female", "Total"];
-        const stack = d3.stack().keys(keys);
-        const series = stack(data);
-
-        const width = 800;
-        const height = 500;
-        const padding = 40;
-
-        const xScale = d3.scaleBand()
-            .domain(data.map(d => d['Country Name']))
-            .range([padding, width - padding])
-            .padding(0.1)
-            .align(0.1);
-
-        const yScale = d3.scaleLinear()
-            .domain([0, d3.max(series, d => d3.max(d, d => d[1]))])
-            .range([height - padding, padding]);
-
-        const svg = d3.select("#chart")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height);
-
-        const groups = svg.selectAll("g")
-            .data(series)
-            .enter()
-            .append("g");
-
-        const tooltip = d3.select("body").append("div")
-            .attr("class", "tooltip")
-            .style("opacity", 0);
-
-        // Update chart based on selected gender
-        window.updateChart = function(gender) {
-            selectedGender = gender;
-            updateBars();
-        };
-
-        function updateBars() {
-            const barColor = getBarColor(selectedGender);
-
-            groups.selectAll("rect")
-                .data(d => d)
-                .transition()
-                .duration(500)
-                .attr("y", d => yScale(d[1]))
-                .attr("height", d => yScale(d[0]) - yScale(d[1]))
-                .attr("fill", barColor);
-
-            svg.select(".y-axis")
-                .transition()
-                .duration(500)
-                .call(d3.axisLeft(yScale));
-
-            svg.selectAll("rect")
-                .on("mouseover", showTooltip)
-                .on("mouseout", hideTooltip);
-        }
-
-        function getBarColor(gender) {
-            if (gender === "Male") {
-                return "#3498db";
-            } else if (gender === "Female") {
-                return "#e74c3c";
-            } else {
-                return "#2ecc71";
-            }
-        }
-
-        const rects = groups.selectAll("rect")
-            .data(d => d)
-            .enter()
-            .append("rect")
-            .attr("x", (d, i) => xScale(data[i]['Country Name']))
-            .attr("y", d => yScale(d[1]))
-            .attr("height", d => yScale(d[0]) - yScale(d[1]))
-            .attr("width", xScale.bandwidth())
-            .attr("fill", d => getBarColor(d.data.gender));
-
-        svg.append("g")
-            .attr("transform", `translate(0, ${height - padding})`)
-            .call(d3.axisBottom(xScale))
-            .selectAll("text")
-            .attr("transform", "rotate(-45)")
-            .style("text-anchor", "end")
-            .attr("fill", "#555");
-
-        svg.append("g")
-            .attr("transform", `translate(${padding}, 0)`)
-            .call(d3.axisLeft(yScale))
-            .selectAll("text")
-            .attr("fill", "#555");
-
-        function showTooltip(event, d) {
-            const value = d.data[selectedGender];
-
-            tooltip.transition()
-                .duration(200)
-                .style("opacity", 0.9);
-            tooltip.html(`${selectedGender}: ${d3.format(",")(value)}`)
-                .style("left", (event.pageX) + "px")
-                .style("top", (event.pageY - 28) + "px");
-        }
-
-        function hideTooltip() {
-            tooltip.transition()
-                .duration(500)
-                .style("opacity", 0);
-        }
-        
-        updateBars(); // Initial update
+    // Load data from CSV
+d3.csv("BarChartDataset.csv").then(function(data) {
+    // Convert string numbers to integers
+    data.forEach(function(d) {
+      d.Male = +d.Male;
+      d.Female = +d.Female;
+      d.Total = +d.Total;
     });
+  
+    // Set up dimensions
+    const margin = { top: 20, right: 20, bottom: 50, left: 50 };
+    const width = 600 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
+  
+    // Create SVG container
+    const svg = d3.select("#chart-container")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+  
+    // Initial class selection
+    let selectedClass = "total";
+  
+    // Update chart based on selected class
+    function updateChart() {
+      const selectedData = data.map(d => ({ Country: d["Country Name"], Value: d[selectedClass] }));
+  
+      // Update scales
+      const xScale = d3.scaleBand()
+        .domain(selectedData.map(d => d.Country))
+        .range([0, width])
+        .padding(0.1);
+  
+      const yScale = d3.scaleLinear()
+        .domain([0, d3.max(selectedData, d => d.Value)])
+        .range([height, 0]);
+  
+      // Update bars
+      const bars = svg.selectAll(".bar")
+        .data(selectedData, d => d.Country);
+  
+      bars.enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", d => xScale(d.Country))
+        .attr("width", xScale.bandwidth())
+        .attr("y", d => yScale(d.Value))
+        .attr("height", d => height - yScale(d.Value))
+        .attr("fill", d => selectedClass === "male" ? "blue" : (selectedClass === "female" ? "pink" : "green"))
+        .on("mouseover", showTooltip)
+        .on("mouseout", hideTooltip);
+  
+      bars.transition()
+        .attr("x", d => xScale(d.Country))
+        .attr("width", xScale.bandwidth())
+        .attr("y", d => yScale(d.Value))
+        .attr("height", d => height - yScale(d.Value))
+        .attr("fill", d => selectedClass === "male" ? "blue" : (selectedClass === "female" ? "pink" : "green"));
+  
+      bars.exit().remove();
+  
+      // Update axes
+      const xAxis = d3.axisBottom(xScale);
+      const yAxis = d3.axisLeft(yScale);
+  
+      svg.select(".x-axis")
+        .attr("transform", `translate(0, ${height})`)
+        .call(xAxis);
+  
+      svg.select(".y-axis")
+        .call(yAxis);
+    }
+  
+    // Initialize chart
+    updateChart();
+  
+    // Handle radio button change
+    d3.selectAll("input[name='class']").on("change", function() {
+      selectedClass = this.value;
+      updateChart();
+    });
+  
+    // Tooltip functions
+    function showTooltip(event, d) {
+      const tooltip = svg.append("g")
+        .attr("class", "tooltip")
+        .style("pointer-events", "none");
+  
+      tooltip.append("rect")
+        .attr("fill", "white")
+        .attr("width", 100)
+        .attr("height", 40)
+        .attr("x", event.x - 50)
+        .attr("y", event.y - 50)
+        .attr("rx", 5)
+        .attr("ry", 5)
+        .attr("stroke", "black")
+        .attr("stroke-width", 1);
+  
+      tooltip.append("text")
+        .attr("x", event.x)
+        .attr("y", event.y - 30)
+        .attr("text-anchor", "middle")
+        .text(`${d.Country}: ${d.Value}`);
+    }
+  
+    function hideTooltip() {
+      svg.select(".tooltip").remove();
+    }
+  });
+  
 }
 
 window.onload = init;
